9494f029fcf69ea7574c1a072496f2bef798e0c8
Ferreyra, Maximiliano Gastón <ferreyra.mg.utnfrba@gmail.com>
2015-04-13 19:15:44 -0300

    Se agrega menu y primer tipo de dato (Int)

---
+++ main.cpp
@@ -0,0 +1,41 @@
+/*
+*   AED - 2015
+*
+*   Autor: Ferreyra, Maximiliano Gastón.
+*
+*   Legajjo: 155911-4.
+*
+*   Fecha: 20150411
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    /*
+    * Declaramos variables que vamos a utilizar durante el programa.
+    */
+
+    int num1 = 37;
+    int num2 = 4;
+
+cout << "===============================================================================\n" << endl;
+cout << "|                          Trabajo Practico No 1 AED                           |" << endl;
+cout << "|                 \"Tipos de Datos y sus Operaciones Basicas\"                   |" << endl;
+cout << "|           Alumno: Ferreyra, Maximiliano Gaston || Legajo: 155911-4           |" << endl;
+cout << "===============================================================================" << endl;
+
+cout << "_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _\n" << endl;
+
+cout << "Tipo de dato: Int" << endl;
+cout << "_________________\n" << endl;
+
+cout << "Suma (num1 + num2): " << num1 + num2 << endl;
+cout << "Resta (num1 - num2): " << num1 - num2 << endl;
+cout << "Multiplicacion (num1 * num2): " << num1 * num2 << endl;
+cout << "Division (num1 / num2): " << num1 / num2 << endl;
+cout << "Resto de Division Entera (num1 % num2): " << num1 % num2 << "\n" << endl;
+}
+
